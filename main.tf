61fafc4cbb89741ec138cf186d9f794ad239fbbc
allan-lee-0724 <allan841@revature.net>
2023-01-12 14:30:19 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+# the name provided in the provider section (see the word in quotation marks) needs to match the name of the required 
+# provider in our terraform.tf file, where we defined our required_provider
+provider "aws" {
+  region     = "us-east-1" # this tells terraform where we want our resources created
+  access_key = var.access_key # this is the access key variable
+  secret_key = var.secret_key # this is the secret key variable
+
+  # both keys should be provided by an IAM role created in aws
+}
+
+module "ec2-instance"{
+    source  = "terraform-aws-modules/ec2-instance/aws"
+    version = "~> 3.0"
+
+    name = "terraform-demo-instance"
+
+    ami                    = "ami-0b5eea76982371e91"
+    instance_type          = "t2.micro"
+    key_name               = "showing-off-keys"
+    monitoring             = false
+    vpc_security_group_ids = ["sg-042d4bcfa8912a50f"] # you can place multiple security groups in your list
+
+    tags = {
+        Terraform   = "true"
+        Environment = "dev"
+    }
+}
